8e26333d28c62254fda9b4ca0c8380c667ef7081
noemie31 <99802192+noemie31@users.noreply.github.com>
2023-02-06 10:50:47 +0100

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import CategorieView from '../views/CategorieView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+    // Associe des chemins d'accès au composant vue à afficher
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      component: CategorieView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
